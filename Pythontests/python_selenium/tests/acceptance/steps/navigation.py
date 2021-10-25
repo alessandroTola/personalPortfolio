7708dd086bc480b39e982cd468d63991d595a4e4
alessandroTola <alessandro.tola@yahoo.it>
2021-10-25 10:54:30 +0200

    [TEST] implement 2 test scenario
    + Homepage can go to Blog
    + Blog can go to Homepage

--- Pythontests/python_selenium/tests/acceptance/steps/navigation.py
+++ Pythontests/python_selenium/tests/acceptance/steps/navigation.py
@@ -1,5 +1,6 @@
 from behave import *
 from selenium.webdriver import Chrome
+
 use_step_matcher('re')
 
 
@@ -8,8 +9,20 @@ def step_impl(context):
     context.browser = Chrome()
     context.browser.get('http://127.0.0.1:5000')
 
+
+@given('I am on the blog page')
+def step_impl(context):
+    context.browser = Chrome()
+    context.browser.get('http://127.0.0.1:5000/blog')
+
+
 @then('I am on the blog page')
 def step_impl(context):
     expected_url = 'http://127.0.0.1:5000/blog'
+    assert context.browser.current_url == expected_url
 
+
+@then('I am on the homepage')
+def step_impl(context):
+    expected_url = 'http://127.0.0.1:5000/'
     assert context.browser.current_url == expected_url
